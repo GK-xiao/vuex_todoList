73670ca16d30dede1efff2063800f56883e55677
jiali <626102624@qq.com>
2020-03-27 13:25:03 +0800

    vuex

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,14 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	  'space-before-function-paren': 0,
+		'no-tabs': 0,
+		'no-mixed-spaces-and-tabs': 0,
+		'space-in-parens': 0,
+		'object-curly-spacing': 0,
+		'no-trailing-spaces': 0,
+		'no-trailing-spaces': 0,
+		'no-unused-vars': 0
   }
 }
